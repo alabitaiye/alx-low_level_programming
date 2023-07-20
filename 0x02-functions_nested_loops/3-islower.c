32b8907b10df06362bf8ee421b5bbbb15d1481f5
Alabi Taiye <alabitaiye894@gmail.com>
2023-07-19 18:36:05 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islowee\r - checks for lowercase character
+ * _islower - checks for lowercase character
  * @c \: The character to be checked
  * Return: 1 for lowercase character of 0 for anything else
  */
