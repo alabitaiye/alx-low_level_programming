062c9bd4b8b701e03cf53e1d0f1e12ca81d83e18
Alabi Taiye <alabitaiye894@gmail.com>
2023-07-21 18:04:01 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,11 +6,13 @@
  * Returned: 1 for uppercase character or 0 for anything else
  */
 int _isupper(int c)
-
 {
 if (c >= 65 && c <= 90)
 {
 return (1);
 }
+else
+{
 return (0);
+}
 }
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* mul - multiplies two integers
+* mut - multiplication function
+* @a: - the first integer
+* @b: - the second integer
+* Return: Always 0
+*/
+
+int mut(int a, int b)
+{
+	int result = a * b;
+	return (result);
+}
